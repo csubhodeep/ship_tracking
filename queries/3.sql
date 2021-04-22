6bca15a5b80a84610c22bfd228db8010a27e555e
csubhodeep <subhodeep.chakraborty@qroda.com>
2021-04-22 13:50:14 +0200

    - add query for task 3

--- queries/3.sql
+++ queries/3.sql
@@ -1,1 +1,11 @@
 /*Show the ranking of the engine types (engine_name) in relation to the maximum speed shown*/
+
+
+SELECT
+    s.engine1_name,
+    MAX(p.SPEED) AS max_speed
+FROM position_data p
+LEFT JOIN ship_engines s
+    USING (SHIP_ID)
+GROUP BY s.engine1_name
+ORDER BY max_speed DESC
