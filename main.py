b57064fb1494c79bd4757ef85b6deb24b5b98e60
csubhodeep <subhodeep.chakraborty@qroda.com>
2021-04-22 17:48:04 +0200

    add logging

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from argparse import ArgumentParser
+import logging
 
 from lib.init_db import DbConnector
 from lib.table_schema import PositionData, ShipEngines, EngineTechSpecs
@@ -8,6 +9,7 @@ from utils.create_table import create_all_tables
 from utils.extraction import get_ship_data, get_engine_tech_specs
 from utils.loading import load_postion_data, load_engine_data, load_owner_data, load_engine_tech_specs
 from utils.transformation import transform_csv, transform_json, transform_ship_owner
+
 
 
 def get_args():
@@ -25,12 +27,17 @@ MMSI_IDS = ["269057500", "269057489"]
 if __name__ == "__main__":
     args = get_args()
 
+    logging.info("====Starting pipeline====")
+
     # initialize the connection to the DB and create all the tables
+    logging.info("Initialising DB")
     db_conn = DbConnector()
     if args.create_or_replace_tables:
         create_all_tables(engine=db_conn.get_engine())
 
+
     # EXTRACT - from marinetraffic.com API
+    logging.info("Starting extraction jobs for position data from third-party API")
     for mmsi in MMSI_IDS:
         config = {
             "API_KEY": os.getenv("API_KEY"),
@@ -44,40 +51,52 @@ if __name__ == "__main__":
         get_ship_data(config=config, path_to_data=PATH_TO_DATA_FOLDER.joinpath(f"position_{config['MMSI']}.json"))
 
     ## TRANSFORM & LOAD
-    schema_position_data = [ele.name for ele in PositionData.c if ele.name != "key"]
+    logging.info("Starting transformation & loading jobs.")
     # 1. CSV data - position
+    schema_position_data = [ele.name for ele in PositionData.c if ele.name != "key"]
     for ele in transform_csv(path_to_data=PATH_TO_DATA_FOLDER.joinpath("position_data.csv"),
-                             schema=schema_position_data):
+                             columns=schema_position_data):
         load_postion_data(ele, connector=db_conn)
     # pass
 
     # 2. JSON data - position
     for mmsi in MMSI_IDS:
         for ele in transform_json(path_to_data=PATH_TO_DATA_FOLDER.joinpath(f"position_{mmsi}.json"),
-                                  schema=schema_position_data):
+                                  columns=schema_position_data):
             if ele:
                 load_postion_data(ele, connector=db_conn)
         # pass
+    logging.info("Transformation and loading completed for position data")
+
 
     # 3. CSV data - engines
     schema_engine_data = [ele.name for ele in ShipEngines.c if ele.name != "key"]
-    for ele in transform_csv(path_to_data=PATH_TO_DATA_FOLDER.joinpath("ship_engines.csv"), schema=schema_engine_data):
+    for ele in transform_csv(path_to_data=PATH_TO_DATA_FOLDER.joinpath("ship_engines.csv"), columns=schema_engine_data):
         load_engine_data(ele, connector=db_conn)
     # pass
+    logging.info("Transformation and loading completed for engine data")
+
 
     # 4. CSV data - owners
     for ele in transform_ship_owner(path_to_data=PATH_TO_DATA_FOLDER.joinpath("ships_per_owner.csv")):
         if ele:
             load_owner_data(ele, connector=db_conn)
     # pass
+    logging.info("Transformation and loading completed for ship-owner data")
+
 
     ## EXTRACT - engine tech specs from webpage
+    logging.info("Webcrawling started for engine tech-specs")
     get_engine_tech_specs(source_uri=WEB_PAGE_ADDRESS, path_to_data=PATH_TO_DATA_FOLDER.joinpath(f"engine_data.json"))
+    logging.info("Webcrawling finished for engine tech-specs")
+
 
     ## LOAD
+    logging.info("Starting transformation & loading job for engine-tech specs")
     schema_engine_tech_specs = [ele.name for ele in EngineTechSpecs.c if ele.name != "key"]
     for ele in transform_json(path_to_data=PATH_TO_DATA_FOLDER.joinpath(f"engine_data.json"),
-                              schema=schema_engine_tech_specs):
+                              columns=schema_engine_tech_specs):
         if ele:
             load_engine_tech_specs(ele, connector=db_conn)
     # pass
+    logging.info("Transformation and loading completed for engine tech-spec data")
